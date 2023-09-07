82fd21097a86f4aa14412423f0488c2cb9fb1fea
Nihal-008 <138968948+Nihal-008@users.noreply.github.com>
2023-09-07 22:19:55 +0530

    new files 7 sept

---
+++ ReverseOfString.java
@@ -0,0 +1,10 @@
+public class ReverseOfString {
+    public static void main(String[] args){
+        String reverseN="Nihal";
+        for(int i=reverseN.length()-1;i>=0;i--){
+        System.out.println(reverseN.charAt(i));
+        }
+
+
+    }
+}
